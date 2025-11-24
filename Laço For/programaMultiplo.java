77bdf81f09ee086e699f86a58a185161c9dc795b
Samuel Fagundes de Souza <samuelfagundes.sza@gmail.com>
2025-11-24 15:02:22 -0300

    Add files via upload

---
+++ Laço For/programaMultiplo.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class programaMultiplo {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 5; i <= 400; i = i + 5) {
+			System.out.println(i);
+		}
+	}
+}
